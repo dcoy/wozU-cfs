5028376acbb13cb6df82b34ec27f2df50c67c055
David Coy <me@david-coy.com>
2017-11-02 07:51:11 -0500

    Setting up flexbox

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-function split(str, delim) {
-  if (typeof delim === "undefined") {
-    return [str];
-  }
 
-  var result = [];
-  if (delim === '') {
-    // split the whole string into an array of letters
-    for (var i = 0, l = str.length; i < l; i++) {
-      result.push(str[i]);
-    }
-    return result;
-  }
+// var textValue = textArea.value;
 
-  var pos = 0;
-  var strClone = str; // never update the passed argument, create a "clone"
+// var avgWords = document.getElementById("avgWords");
+// var totalSpaces = document.getElementById("totalSpaces");
+// var totalWords = document.getElementById("totalWords");
+// var totalSent = document.getElementById("totalSent");
 
-  pos = strClone.indexOf(delim);
-  while (pos > -1) {
-    // get word
-    var word = strClone.substr(0, pos);
+// function splitInput(str, delim) {
+//   if (typeof delim === "undefined" | "null") {
+//     return [str];
+//   }
 
-    // include word on our results array
-    result.push(word);
+//   var result = [];
+//   if (delim === '') {
+//     // split the whole string into an array of letters
+//     for (var i = 0, l = str.length; i < l; i++) {
+//       result.push(str[i]);
+//     }
+//     return result;
+//   }
 
-    // change string (not the original one, just a clone) to remove the word that we found before (+ delimiter length)
-    strClone = strClone.substr(word.length + delim.length);
+//   var pos = 0;
+//   var strClone = str; // never update the passed argument, create a "clone"
 
-    // update pos so while won't be infinite
-    pos = strClone.indexOf(delim);
-  }
+//   pos = strClone.indexOf(delim);
+//   while (pos > -1) {
+//     // get word
+//     var word = strClone.substr(0, pos);
 
-  // push the last result (while finish before the last result, so we have to do this manually)
-  result.push(strClone);
-  return result;
+//     // include word on our results array
+//     result.push(word);
+
+//     // change string (not the original one, just a clone) to remove the word that we found before (+ delimiter length)
+//     strClone = strClone.substr(word.length + delim.length);
+
+//     // update pos so while won't be infinite
+//     pos = strClone.indexOf(delim);
+//   }
+
+//   // push the last result (while finish before the last result, so we have to do this manually)
+//   result.push(strClone);
+//   return result;
+// }
+
+window.onload = function() {
+  var input = document.getElementById("text-input");
+  console.log(input);
 }
